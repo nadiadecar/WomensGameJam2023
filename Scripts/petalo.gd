e4df5d87f6da5312455c5c8283411f96e6869e77
nadiadecar <nadiadecar@ug.uchile.cl>
2023-08-12 19:18:04 -0400

    Se agrega la base de la escena dos con el movimiento de los petalos

---
+++ Scripts/petalo.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+
+var seleccionado = false
+var cae = false
+
+
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("click"):
+		seleccionado = true
+
+
+func _physics_process(delta):
+	if seleccionado: 
+		global_position = lerp(global_position, get_global_mouse_position(), 25*delta )
+	if cae:
+		global_position = lerp(global_position, Vector2(global_position.x, 900), 10*delta)
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed and seleccionado: 
+			seleccionado = false 
+			cae = true
+				
+			
