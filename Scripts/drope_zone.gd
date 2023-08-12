2484a0d91e13c68439a1b05a24082676ff787a5d
nadiadecar <nadiadecar@ug.uchile.cl>
2023-08-12 17:15:56 -0400

    Ya funciona de forma básica el drag and drop

---
+++ Scripts/drope_zone.gd
@@ -0,0 +1,16 @@
+extends Marker2D
+
+func _draw():
+	var puntos = PackedVector2Array()
+	puntos = [Vector2(100,100), Vector2(-100,100), Vector2(-100,-100), Vector2(100,-100)]
+	var color = PackedColorArray()
+	color = [Color.AZURE]
+	draw_polygon(puntos, color )
+
+func seleccionado():
+	for child in get_tree().get_nodes_in_group("zonas"):
+		child.deseleccionado()
+	modulate = Color.ANTIQUE_WHITE
+
+func deseleccionado():
+	modulate = Color.CHOCOLATE
---
+++ Scripts/piezas_puzzle.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+var seleccionado = false
+var espacios
+var punto_soltado = Vector2(100,100)
+
+func _ready(): 
+	espacios = get_tree().get_nodes_in_group("zonas")
+
+
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("click"):
+		seleccionado = true
+
+
+func _physics_process(delta):
+	if seleccionado: 
+		global_position = lerp(global_position, get_global_mouse_position(), 25*delta )
+	else:
+		global_position = lerp(global_position, punto_soltado, 10*delta)
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed: 
+			seleccionado = false 
+			var minima_distancia = 75
+			for child in espacios: 
+				var distancia = global_position.distance_to(child.global_position)
+				if distancia < minima_distancia:
+					child.seleccionado()
+					punto_soltado = child.global_position
+					minima_distancia = distancia
+				
+			
