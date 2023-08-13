f5a524cb63d09232e000e8baf2c714538466d17f
nadiadecar <nadiadecar@ug.uchile.cl>
2023-08-12 21:51:19 -0400

    Los cambios de iluminación quedan listos

--- Scripts/cosas_cajas_escena.gd
+++ Scripts/cosas_cajas_escena.gd
@@ -5,15 +5,29 @@ extends Node2D
 @onready var jade_objetos = get_tree().get_nodes_in_group("jade_objetos")
 @onready var noa_objetos = get_tree().get_nodes_in_group("noa_objetos")
 
+var turnos = {"jade": "noa", "noa": "jade"}
 var turno = 'jade'
 
 func _ready():
 	for child in jade_objetos: 
-		child.en_caja.connect(objeto_en_caja)
+		child.en_caja.connect(objeto_en_caja_funcion)
 	for child in noa_objetos: 
-		child.en_caja.connect(objeto_en_caja)
+		child.en_caja.connect(objeto_en_caja_funcion)
+	cambiar_colores()
 
 func _physics_process(delta):
+	pass
+
+func objeto_en_caja_funcion(objeto): 
+	print("se ve la señal")
+	objeto.queue_free()
+	cambiar_colores()
+
+func cambiar_colores():
+	turno = turnos[turno]
+	jade = get_tree().get_nodes_in_group("jade")
+	noa = get_tree().get_nodes_in_group("noa")
+	
 	if turno == 'jade':
 		for child in jade: 
 			if not child.is_in_group('zonas'):
@@ -21,6 +35,7 @@ func _physics_process(delta):
 		for child in noa: 
 			if not child.is_in_group('zonas'):
 				child.no_seleccionable()
+	
 	if turno == 'noa':
 		for child in jade: 
 			if not child.is_in_group('zonas'):
@@ -28,7 +43,4 @@ func _physics_process(delta):
 		for child in noa: 
 			if not child.is_in_group('zonas'):
 				child.seleccionable()
-
-func objeto_en_caja(objeto): 
-	print("se ve la señal")
-	objeto.visible = false
+	
